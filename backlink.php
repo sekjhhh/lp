837c00306b97e89fd8b53dc3ae3e0d7051f28354
sekjhhh <165985383+sekjhhh@users.noreply.github.com>
2024-04-05 13:23:35 +0700

    Create backlink.php

---
+++ backlink.php
@@ -0,0 +1,10 @@
+   <div style="display: none;">
+<a href="https://disdikbudkabbatang.com/lib/">ibu4d</a>
+<a href="http://sismadak.rsudrsoetomo.jatimprov.go.id/logut.html">slot 4d</a>
+<a href="https://e-ppid.kemlu.go.id/storage/pro/">slot gacor</a>
+<a href="https://dlh.oganilirkab.go.id/assets/pro/">bandar togel</a>
+<a href="https://sim.unusa.ac.id/main.html">agen togel</a>
+<a href="https://jaki.pta-bandung.go.id/main.html">toto togel</a>
+<a href="https://aursati.desa.id/">toto slot 4d</a>
+
+     </div>
