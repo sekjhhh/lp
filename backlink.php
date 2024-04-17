3673fcb65ec0dc112aef718536e7055dbd5017e7
sekjhhh <165985383+sekjhhh@users.noreply.github.com>
2024-04-17 13:16:18 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,6 +1,6 @@
    <div style="display: none;">
 <a href="http://157.245.62.54/anjab-manggarai-barat/files/jp.html">slot server thailand</a>
-<a href="http://sismadak.rsudrsoetomo.jatimprov.go.id/logut.html">slot 4d</a>
+<a href="https://skopi.kemenkopukm.go.id/images/pt/index.php">slot server thailand</a>
 <a href="https://e-ppid.kemlu.go.id/storage/pro/">slot gacor</a>
 <a href="https://dlh.oganilirkab.go.id/assets/pro/">bandar togel</a>
 <a href="https://sim.unusa.ac.id/main.html">agen togel</a>
