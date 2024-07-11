0bf16957f4a884f44dfd478b504d879a271a65a8
sekjhhh <165985383+sekjhhh@users.noreply.github.com>
2024-07-11 21:40:59 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -4,7 +4,7 @@
 <a href="https://pusdasip.kedirikota.go.id/files_skp_2024/gacorr/">slot gacor</a>
 <a href="https://dlh.oganilirkab.go.id/assets/pro/">bandar togel</a>
 <a href="https://sim.unusa.ac.id/main.html">agen togel</a>
-<a href="https://jaki.pta-bandung.go.id/main.html">toto togel</a>
+<a href="https://spd.bawaslu.go.id/ams-panel/sg/">slot gacor</a>
 <a href="https://aursati.desa.id/">toto slot 4d</a>
 <a href="https://kemenagkotabaubau.com/file/img/">slot 4d</a>
 <a href="https://law.unib.ac.id/wp-content/themes/zgacor/">toto slot</a>
