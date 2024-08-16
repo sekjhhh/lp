b87eb24d383f29f73b102a9a3e080b9548e30d55
sekjhhh <165985383+sekjhhh@users.noreply.github.com>
2024-08-16 07:01:51 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,8 +1,8 @@
 <div style="display:none">
   <h2 style="position:absolute;top:-12540px">
-  <li><a href="https://speed.sipakatau.iainpalopo.ac.id/jss/" rel="dofollow">slot gacor</a><li>
-  <li><a href="https://hadir.jembranakab.go.id/file/" rel="dofollow">slot gacor 4d</a><li>
-  <li><a href="http://157.245.80.103/win/" rel="dofollow">slot gacor 4d</a><li>
+  <li><a href="https://siakad.unipi.ac.id/uploads/tgl/" rel="dofollow">win168</a><li>
+  <li><a href="https://e-planning.klaten.go.id/" rel="dofollow">slot bonus 100</a><li>
+  <li><a href="https://bersinar2045.klaten.go.id/rp/" rel="dofollow">slot gacor 2024</a><li>
   <li><a href="https://sima.uniramalang.ac.id/front/gate/win/" rel="dofollow">slot gacor 2024</a><li>
   <li><a href="https://sijago.dephub.go.id/js/jss/" rel="dofollow">slot gacor</a><li>
   <li><a href="https://kesbangpol.balikpapan.go.id/wp-content/uploads/img/" rel="dofollow">slot gacor</a><li>
@@ -10,5 +10,9 @@
   <li><a href="https://holymadness.com/" rel="dofollow">win168win</a><li>
   <li><a href="http://terlaksana.co.id/gambar/pdf/" rel="dofollow">win168</a><li>
   <li><a href="https://www.saharaclinic.co.id/wp-content/js/" rel="dofollow">slot gacor</a><li>
+  <li><a href="https://siakad.unipi.ac.id/uploads/tgl/" rel="dofollow">win168</a><li>
+  <li><a href="https://siakad.unipi.ac.id/uploads/tgl/" rel="dofollow">win168</a><li>
+  <li><a href="https://siakad.unipi.ac.id/uploads/tgl/" rel="dofollow">win168</a><li>
+  <li><a href="https://siakad.unipi.ac.id/uploads/tgl/" rel="dofollow">win168</a><li>
 
       </div>
