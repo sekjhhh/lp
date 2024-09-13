5fd20a8d0d7120bfa778534eaf00f61a0fd00f29
sekjhhh <165985383+sekjhhh@users.noreply.github.com>
2024-09-13 14:16:23 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -7,7 +7,7 @@
 <li><a href="https://ibsaude.org.br/class/data/" rel="dofollow">Slot Thailand</a><li>
 <li><a href="https://e-surat.sid.net.id/images/gacor/" rel="dofollow">Slot Gacor</a><li>
 <li><a href="https://e-surat.sid.net.id/images/gacor/" rel="dofollow">slot gacor</a><li>
-<li><a href="https://lkr.esakip.net/pdf/win/" rel="dofollow">Slot Thailand</a><li>
+<li><a href="https://e-surat.sid.net.id/images/gacor/" rel="dofollow">Slot gacor</a><li>
 <li><a href="https://itaps-fem.ipb.ac.id/wp-content/cace/" rel="dofollow">Slot Gacor</a><li>
 <li><a href="https://hangoverloungebali.com/play/" rel="dofollow">judi bola 365</a><li>
 <li><a href="https://dib.alirsyadbandung.sch.id/storage/gacorr/" rel="dofollow">toto slot</a><li>
