bf39b24ff8c5bfdce6fd03eabcc6632d02512de9
sekjhhh <165985383+sekjhhh@users.noreply.github.com>
2024-10-04 12:56:03 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -5,9 +5,8 @@
 <li><a href="https://si-pelangsing.badungkab.go.id/phpMyAdmin/pdf/" rel="dofollow">slot gacor</a><li>
 <li><a href="https://bakesbangpol.katingankab.go.id/js/kbt/" rel="dofollow">kembangtoto</a><li>
 <li><a href="https://ppid.bpkh.go.id/wp-content/themes/news/" rel="dofollow">Slot Thailand</a><li>
-<li><a href="https://ipd-ac.paidafrica.org/elearning/jwbart/" rel="dofollow">JAWABARAT4D</a><li>
+<li><a href="https://billing.sid.net.id/assets/bar/" rel="dofollow">slot gacor</a><li>
 <li><a href="https://e-surat.sid.net.id/images/gacor/" rel="dofollow">slot gacor</a><li>
-<li><a href="https://slot-thailand.smkjb.id/ rel="dofollow">Slot thailand</a><li>
 <li><a href="https://fiki.politeknikpalu.ac.id/jquri/" rel="dofollow">Slot Gacor</a><li>
 <li><a href="https://hangoverloungebali.com/play/" rel="dofollow">judi bola 365</a><li>
 <li><a href="https://si-pelangsing.badungkab.go.id/assets/file/gacorr/" rel="dofollow">toto slot</a><li>
